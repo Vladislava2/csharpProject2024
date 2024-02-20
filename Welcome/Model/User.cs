78dbc02c5b6e92913a288dfb50e00244ec10f828
Student <Student@CIR2108A-11>
2024-02-20 15:16:55 +0200

    Add project files.

---
+++ Welcome/Model/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Welcome.Othesr;
+
+namespace Welcome.Model
+{
+    internal class User
+    {
+        private string names;
+
+        public string Names { get => names; set => names = value; }
+        private string password;
+        public string Password { get => password; set => password = value; }
+        private UserRolesEnum role;
+        public UserRolesEnum Role { get => role; set => role = value; }
+        
+    }
+}
---
+++ Welcome/ViewModel/UserViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Welcome.Model;
+using Welcome.Othesr;
+
+namespace Welcome.ViewModel
+{
+    internal class UserViewModel
+    {
+        private User _user;
+
+        public string Names
+        {
+            get { return _user.Names; }
+            set { _user.Names = value; }
+        }
+        public string Password
+        {
+            get { return _user.Password; }
+            set { _user.Password = value; }
+        }
+        public UserRolesEnum Role
+        {
+            get { return _user.Role; }
+            set
+            {
+                _user.Role = value;
+            }
+        }
+    }
+}
